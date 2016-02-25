c56aa12e9ea14e47a2fd94446e5d0882b300f7ce
alexandregama <alexandre.gama.lima@gmail.com>
2016-02-25 08:55:38 -0300

    Creates a HATEOAS link to enable user to call the next resource with its action

--- src/main/java/com/payfast/endpoint/payment/HashMapPaymentsDao.java
+++ src/main/java/com/payfast/endpoint/payment/HashMapPaymentsDao.java
@@ -1,14 +1,17 @@
 package com.payfast.endpoint.payment;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class HashMapPaymentsDao implements Payments {
 
-	private Map<Long, Payment> database = new HashMap<>();
+	private static Map<Long, Payment> database = new HashMap<>();
 	
-	public HashMapPaymentsDao() {
+	static {
 		database.put(1L, new Payment(1L, "INPROGRESS"));
 		database.put(2L, new Payment(2L, "INPROGRESS"));
 		database.put(3L, new Payment(3L, "CANCELED"));
@@ -27,9 +30,25 @@ public class HashMapPaymentsDao implements Payments {
 		Long nextId = database.keySet().stream().max(Long::compareTo).get() + 1;
 		
 		payment.setId(nextId);
+		payment.started();
 		database.put(nextId, payment);
 		
 		return payment;
+	}
+
+	@Override
+	public Payment confirm(Payment payment) {
+		Payment confirmedPayment = database.get(payment.getId());
+		confirmedPayment.setStatus(payment.getStatus());
+		database.put(payment.getId(), confirmedPayment);
+		return null;
+	}
+
+	@Override
+	public List<Payment> listAll() {
+		Collection<Payment> values = database.values();
+		
+		return new ArrayList<>(values);
 	}
 
 }
--- src/main/java/com/payfast/endpoint/payment/Payments.java
+++ src/main/java/com/payfast/endpoint/payment/Payments.java
@@ -1,5 +1,6 @@
 package com.payfast.endpoint.payment;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface Payments {
@@ -7,5 +8,9 @@ public interface Payments {
 	Optional<Payment> findBy(Long id);
 
 	Payment saveNew(Payment payment);
+
+	Payment confirm(Payment payment);
+
+	List<Payment> listAll();
 	
 }
